ef83fbe337adb4da8ed321c994cd5fdcf76f914f
dbxodnsms <57580401+dbxodnsms@users.noreply.github.com>
2019-11-10 14:33:56 +0900

    Add mongoose

--- init.js
+++ init.js
@@ -1,5 +1,14 @@
 var express = require("express");
+var mongoose = require("mongoose");
 var app = express();
+
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser:true});
+
+var db = mongoose.connection;
+db.on('error',console.error.bind(console, 'connection error:'));
+db.once('open',function(){
+    console.log("Db연결됨")
+});
 
 app.get("/", function(req, res) {
   res.send("여기는 가톨릭 대학교~");
