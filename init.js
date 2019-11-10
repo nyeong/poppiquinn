d5169aaf215e5a70fefb5be40cb43dd313e731e5
An Nyeong <me@annyeong.me>
2019-11-10 13:16:59 +0900

    Update init.js

--- init.js
+++ init.js
@@ -1,10 +1,12 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/', function (req,res) {
-    res.send('Hello World!');
+app.get("/", function(req, res) {
+  res.send("여기는 가톨릭 대학교~");
+  console.log("누군가가 접속했습니다:");
+  console.log(">", req.headers["user-agent"]);
 });
 
-app.listen(3000, function(){
-    console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
 });
